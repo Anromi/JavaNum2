d8ac19086d3ebed900ca08b33b05e89a0ae39ab5
Anromi <54954473+Anromi@users.noreply.github.com>
2020-03-15 16:34:57 +0300

    test1

---
+++ scr/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner str = new Scanner(System.in);
+        System.out.println("Введите текст: ");
+        System.out.println(str.next());
+    }
+}
+
